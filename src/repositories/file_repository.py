353e0cad8caee82831a476e587f38230db57d96e
TempleDB <templedb@localhost>
2026-09-19 21:07:33 -0500

    TempleDB materialize (765 files)

--- src/repositories/file_repository.py
+++ src/repositories/file_repository.py
@@ -42,6 +42,7 @@ class FileRepository(BaseRepository):
                     pf.file_path,
                     pf.file_name,
                     pf.lines_of_code,
+                    pf.edit_mode,
                     fc.content_hash,
                     fc.version,
                     cb.content_text,
